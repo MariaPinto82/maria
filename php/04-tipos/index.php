9d1ca40bf3d49bacbb6f757945571cbaa57aa461
MariaPinto <maria.fullstack@gmail.com>
2021-04-13 19:29:39 +0200

    tipo d datos

---
+++ php/04-tipos/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Tipos de datos:
+ * Integer: Los INT o enteros pueden tener distintos valores numericos enteros que se expresan con diferentes notaciones
+ * 
+ * Float o Double: Este tipo de datos son los numeros de punto flotante o los que llamariamos "numeros decimales", por ejemplo: 9.876;
+ * Ambos tienen mucha precisión, pero double es el más preciso.
+ * 
+ * String: El tipo de datos String tambien conocido como cadena de caracteres.
+ * 
+ * Boolean: Se trata de un tipo logico. Sus posibles valores son true (verdadero) o false (falso);
+ * 
+ */
