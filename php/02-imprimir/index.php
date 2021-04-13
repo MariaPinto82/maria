0844fe20ea81d7a367345ab51499cfa9e3bdec29
MariaPinto <maria.fullstack@gmail.com>
2021-04-13 18:32:23 +0200

    first commit

---
+++ php/02-imprimir/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP imprimir por pantalla</title>
+</head>
+<body>
+    <h1>Master en php</h1>
+
+    <?php
+
+    echo "<h3>Listado de videojuegos:</h3>";
+    echo "<ul>"
+        ."<li>GTA</li>"
+        ."<li>CALL</li>"
+        ."<li>Sims 4</li>"
+        ."</ul>";
+        
+    ?>
+
+
+</body>
+</html>
+
+
+
