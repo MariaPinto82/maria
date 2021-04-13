111bb4aad06dc639c79faaa5fcbf43dbdbb77781
MariaPinto <maria.fullstack@gmail.com>
2021-04-13 19:17:23 +0200

    creando variables

--- php/03-variables/index.php
+++ php/03-variables/index.php
@@ -1,5 +1,10 @@
 <?php
 // Una variable es un contenedor de información, puedo guardar cualquier dato dentro de ella; 
 $miPrimeraVariable = "Hola mundo desde una variable";  // Aqui declaramos nuestra primera variable con el signo dolar;
+$numero = 44;
+$verdadero = true;
 echo $miPrimeraVariable; // Mostramos que contiene la variable;
-
+echo "<br>";
+echo $numero; 
+echo "<br>";
+echo $verdadero; 
