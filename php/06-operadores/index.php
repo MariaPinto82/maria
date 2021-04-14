99fb242c764168cfdc148acbc9743a8fe3ebb9c2
MariaPinto <maria.fullstack@gmail.com>
2021-04-14 13:16:51 +0200

    operadores

---
+++ php/06-operadores/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Operadores aritmeticos:
+ * Suma : $a + $b;
+ * Resta: $a - $b;
+ * Multiplicación: $a * $b;
+ * División: $a / $b;
+ * Resto o Modulo: $a % $b;
+ */
+
+
+
+$numero1 = 55;
+$numero2 = 33;
+
+echo 'Suma: '.($numero1 + $numero2);
+echo "<hr>";
+echo 'Resta: '.($numero1 - $numero2);
+echo "<hr>";
+echo 'Multiplicación: '.($numero1 * $numero2);
+echo "<hr>";
+echo 'División: '.($numero1 / $numero2);
+echo "<hr>";
+echo 'Resto: '.($numero1 % $numero2);
+echo "<hr>";
+
+
+
+/**
+ * Operadores de Incremento y Decremento;
+ * Pre-incremento: ++$a;
+ * Post-incremento: $a++;
+ * Pre-decremento: --$a;
+ * Post-decremento: $a--;
+ */
+
+
+
