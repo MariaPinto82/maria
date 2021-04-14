b130e0531a6eaf25d8d124215da1ad7ee494f58f
MariaPinto <maria.fullstack@gmail.com>
2021-04-14 21:23:47 +0200

    operadores de asignacion

--- php/06-operadores/index.php
+++ php/06-operadores/index.php
@@ -34,5 +34,35 @@ echo "<hr>";
  * Post-decremento: $a--;
  */
 
+$year = 2019;
+
+$year++;
+$year--;
+++$year;
+--$year;
+echo "<h1>$year</h1>";
 
 
+/**
+ * Operadores de asignación:
+ * = : $a = $b (asigna el valor de la variable $b a la variable $a);
+ * += : $a += $b (Asigna el valor de la suma de las variables $a y $b a la variable $a. Equivale a, $a = $a+$b);
+ * -= : $a -= $b (Asigna el valor de la resta de las variables $a y $b a la variable $a. Equivale a, $a = $a-$b);
+ * *= : $a *= $b (Asigna el valor de la multiplicacion de las variables $a y $b a la variable $a. Equivale a, $a = $a*$b);
+ * /= : $a /= $b (Asigna el valor de la division de las variables $a y $b a la variable $a. Equivale a, $a = $a = $a/$b);
+ * %= : $a %= $b (Asigna el valor del módulo de la variable $a y $b a la variable $a. Equivale a, $a = $a%$b);
+ * .= : $a .= $b (Asigna el valor de la concatenacion de las variables $a y $b a la variable $a. Equivale a, $a = $a . $b)
+ */
+
+
+$edad = 1234;
+
+echo $edad."<br>";
+
+echo ($edad+=5)."<br>"; 
+
+echo ($edad-=5)."<br>";
+
+echo ($edad*=5)."<br>";
+
+echo ($edad/=5)."<br>";
