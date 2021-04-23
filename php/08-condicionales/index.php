0479ce3afc7916fc42b971b9e00a61d660a6aa59
MariaPinto <maria.fullstack@gmail.com>
2021-04-23 22:51:20 +0200

    if else

--- php/08-condicionales/index.php
+++ php/08-condicionales/index.php
@@ -68,3 +68,29 @@ if($edad >= $mayoriaEdad){
     echo "<h3>$nombre es menor de edad </h3>";
 }
 
+// Ejemplo 4:
+$dia = 5;
+
+if($dia == 1){
+    echo "Es lunes";
+}else{
+    if($dia == 2){
+        echo "Es martes";
+    }else{
+        if($dia == 3){
+            echo "Es miercoles";
+        }else{
+            if($dia == 4){
+                echo "Es jueves";
+            }else{
+                if($dia == 5){
+                    echo "Es viernes";
+                }else{
+                    if($dia == 6){
+                        echo "Es fin de semana";
+                    }
+                }
+            }
+        }
+    }
+}
