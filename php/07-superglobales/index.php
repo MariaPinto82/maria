1b5c9e928c4619262f80521bd9e8d21bc98dcd48
MariaPinto <maria.fullstack@gmail.com>
2021-04-23 18:07:56 +0200

    variables superglobales

--- php/07-superglobales/index.php
+++ php/07-superglobales/index.php
@@ -11,3 +11,19 @@
  * $_REQUEST;
  * $_ENV;
  */
+
+echo '<h1>';
+echo $_SERVER['SERVER_NAME'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_SOFTWARE'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['HTTP_USER_AGENT'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['REMOTE_ADDR'];
+echo '</h1>';
