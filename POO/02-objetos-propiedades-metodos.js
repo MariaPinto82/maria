330783d488c3b39b6d66e31dffd57bcc80c4f76d
MariaPinto <maria.fullstack@gmail.com>
2021-04-30 00:02:24 +0200

    date

--- POO/02-objetos-propiedades-metodos.js
+++ POO/02-objetos-propiedades-metodos.js
@@ -12,9 +12,24 @@ let object = {
     // El método es una función dentro de un objeto.
     descripcion: function(){
         console.log("Su nombre es "+object.nombre+" y tiene "+object.edad+" años.");
+    },
+    saludar: function(saludo){
+        console.log(saludo+" "+object.nombre)
     }
 
 
 }
 console.log(object.nombre);
 object.descripcion();   // Aqui llamamos al método
+object.saludar("Hola");
+
+
+/*====================================================================================================================
+                                            Objetos Primitivos
+=====================================================================================================================*/
+
+// El objeto Date ae utiliza para trabajar con fechas y horas
+
+let d = new Date();   // Saca la fecha y hora actual
+console.log("d", d)
+
