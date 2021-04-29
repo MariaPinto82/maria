e8469ca5c6e743f939f69140ba0f164f7a88eb57
MariaPinto <maria.fullstack@gmail.com>
2021-04-29 17:40:42 +0200

    master en js

---
+++ javascript/js/variables.js
@@ -0,0 +1,27 @@
+'use strict';
+// Variables Numericas
+let numero = 5;
+console.log("numero:", numero);
+
+
+// Variables de Texto
+let palabra = "hola";
+console.log("palabra:", palabra);
+
+// Variables Booleanas
+
+const booleana = true;
+console.log("booleana:", booleana);
+
+// Variables de tipo Array
+let colores = ["rojo", "amarillo", "azul"];
+console.log("colores:", colores);
+
+// Variables de tipo objeto
+let zumo = {
+    "ingrediente1":"fressa",
+    "ingrediente2":"mandarina",
+    "ingrediente3":"banana"
+}
+
+console.log("zumo:", zumo.ingrediente1)
