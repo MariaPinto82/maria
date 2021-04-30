ed76e364fcf38f0922e269072aced66a5c877f69
MariaPinto <maria.fullstack@gmail.com>
2021-04-30 23:35:29 +0200

    caso 2 ejercicios javascript

--- POO/03-ejercicios-de-logica.js
+++ POO/03-ejercicios-de-logica.js
@@ -19,21 +19,21 @@ let a = {
             a.D > a.C &&
             a.D < a.A) {
 
-                return true;
-            }
+            return true;
+        }
 
-            return false;
+        return false;
     },
-    intervalo: setInterval(function(){
+    intervalo: setInterval(function () {
 
 
 
-        a.A = Math.ceil(Math.random()*4);
-        a.B = Math.ceil(Math.random()*4);
-        a.C = Math.ceil(Math.random()*4);
-        a.D = Math.ceil(Math.random()*4);
+        a.A = Math.ceil(Math.random() * 4);
+        a.B = Math.ceil(Math.random() * 4);
+        a.C = Math.ceil(Math.random() * 4);
+        a.D = Math.ceil(Math.random() * 4);
 
-        if(a.resultado()){
+        if (a.resultado()) {
             clearInterval(a.intervalo);
             console.log("atleta A", a.A);
             console.log("atleta B", a.B);
@@ -46,30 +46,70 @@ let a = {
 
 }
 
-
-
-
-
 /*====================================================================================================================
 Caso 2: Caballos
 
--El caballo de Mac es mas oscuro que el de smith, pero mas rapido y mas viejo que el de Jack,
-que es aun mas lento que el de Willy, que es mas joven que el de Mac, que es mas viejo que el
-de Smith, que es mas claro que el de Willy, aunque el de Jack es mas lento y mas oscuro que el
-de Smith. ¿Cual es el mas viejo, cual el mas lento y cual el mas claro?
+-El caballo de Mac es más oscuro que el de Smith, pero más rapido y más viejo que el de Jack,
+que es aun más lento que el de Willy, que es màs joven que el de Mac, que es màs viejo que el
+de Smith, que es más claro que el de Willy, aunque el de Jack es más lento y más oscuro que el
+de Smith. ¿Cual es el más viejo, cual el más lento y cual el más claro?
+
+Mac | edad : viejo (2) | velocidad: rapido (2) | color: oscuro (2);
+Smith | edad : joven (1) | velocidad: rapido(2) | color: claro (1);
+Jack | edad : joven (1) | velocidad: lento (1) | color: oscuro (2);
+Willy | edad : joven (1) | velocidad: rapido (2) | color: oscuro (2);
 =====================================================================================================================*/
 
 
+let b = {
+    Mac: { edad: 0, velocidad: 0, color: 0 },
+    Smith: { edad: 0, velocidad: 0, color: 0 },
+    Jack: { edad: 0, velocidad: 0, color: 0 },
+    Willy: { edad: 0, velocidad: 0, color: 0 },
+    resultado: function () {
+        if (b.Mac.color > b.Smith.color &&
+            b.Mac.velocidad > b.Jack.velocidad &&
+            b.Mac.edad > b.Jack.edad &&
+            b.Willy.velocidad > b.Jack.velocidad &&
+            b.Mac.edad > b.Willy.edad &&
+            b.Mac.edad > b.Smith.edad &&
+            b.Willy.color > b.Smith.color &&
+            b.Smith.velocidad > b.Jack.velocidad &&
+            b.Jack.color > b.Smith.color
+            ){
+                return true;
+            }
+            return false;
+    },
+    intervalo: setInterval(function(){
+        b.Mac.edad = Math.ceil(Math.random()*2);
+        b.Mac.velocidad = Math.ceil(Math.random()*2);
+        b.Mac.color = Math.ceil(Math.random()*2);
+
+        b.Smith.edad = Math.ceil(Math.random()*2);
+        b.Smith.velocidad = Math.ceil(Math.random()*2);
+        b.Smith.color = Math.ceil(Math.random()*2);
+
+        b.Jack.edad = Math.ceil(Math.random()*2);
+        b.Jack.velocidad = Math.ceil(Math.random()*2);
+        b.Jack.color = Math.ceil(Math.random()*2);
+
+        b.Willy.edad = Math.ceil(Math.random()*2);
+        b.Willy.velocidad = Math.ceil(Math.random()*2);
+        b.Willy.color = Math.ceil(Math.random()*2);
+        if(b.resultado()){
+            clearInterval(b.intervalo);
+            console.log("Caballo de Mac", b.Mac);
+            console.log("Caballo de Smith", b.Smith);
+            console.log("Caballo de Jack", b.Jack);
+            console.log("Caballo de Willy", b.Willy);
+        }
+    }, 10)
+}
 
 
 
-/*====================================================================================================================
-Caso 3: Los Cuatro Perros
 
--Tenemos cuatro perros: un glago, un dogo, un alano y un podenco. Este ultimo come mas que el galgo;
-el alano como mas que el galgo y menos que el dogo, pero este come mas que el podenco. ¿Cual de los
-cuatro perros come menos?
-=====================================================================================================================*/
 
 
 
