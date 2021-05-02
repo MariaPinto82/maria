f39f65ba4cf0a497632f90c050f5052397f9dd0c
MariaPinto <maria.fullstack@gmail.com>
2021-05-02 17:49:42 +0200

    copia de seguridad

--- calculadora/js/script.js
+++ calculadora/js/script.js
@@ -1,16 +1,52 @@
-const p = {
+/**
+ * Objeto con las propiedades de la calculadora
+ */
 
-    teclas: document.querySelectorAll("#calculadora ul li")
+let p = {
+    teclas: document.querySelectorAll("#calculadora ul li"),
+    accion: null
 }
 
 
 
-const m = {
 
+/**
+ * Objeto con los metodos de la calculadora
+ */
+
+let m = {
     inicio: function() {
-        console.log("hola")
+        for (let i = 0; i < p.teclas.length; i++) {
+            console.log("i", i)
+            p.teclas[i].addEventListener("click", m.pulsar)
+        }
+    },
+    pulsar: function(e) {
+        p.accion = e.target.getAttribute("class")
+        m.calculadora(p.accion);
+    },
+    calculadora: function(accion) {
+        switch (accion) {
+            case "numero":
+                console.log("numero");
+                break;
+
+
+            case "signo":
+                console.log("signo");
+                break;
+
+            case "decimal":
+                console.log("decimal");
+                break;
+
+
+            case "igual":
+                console.log("igual");
+                break;
+
+        }
     }
 }
 
-
-m.inicio()
+m.inicio();
