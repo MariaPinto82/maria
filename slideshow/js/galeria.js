23d7d776090bd98d10eb25bc0ff8661534122d72
MariaPinto <maria.fullstack@gmail.com>
2021-05-08 00:17:24 +0200

    creando el fondo oscuro para mi galeria

--- slideshow/js/galeria.js
+++ slideshow/js/galeria.js
@@ -1,23 +1,37 @@
-/*==========================================================================================================================
-                                Objeto con las propiedades de la galeria
-==========================================================================================================================*/
 let pg = {
-    imgGaleria: document.querySelector("#galeria ul li img")
+    imgGaleria: document.querySelectorAll("#galeria ul li img"),
+    rutaImagen: null,
+    cuerpoDom: document.querySelector("body"),
+    lightbox: null,
+    modal: null
 }
 
 
-
-
-
-
-
-/*===========================================================================================================================
-                                Objeto con los métodos de la galeria
-============================================================================================================================*/
-
 let mg = {
     inicioGaleria: function() {
-        pg.imgGaleria
+        for (let i = 0; i < pg.imgGaleria.length; i++) {
+            pg.imgGaleria[i].addEventListener("click", mg.capturaImagen)
+        }
+    },
+    capturaImagen: function(img) {
+        pg.rutaImagen = img.target;
+        mg.lightbox(pg.rutaImagen)
+    },
+    lightbox: function(img) {
+        pg.cuerpoDom.appendChild(document.createElement("DIV")).setAttribute("id", "lightbox");
+        pg.lightbox = document.querySelector("#lightbox");
+
+
+        pg.lightbox.style.width = "100%";
+        pg.lightbox.style.height = "100%";
+        pg.lightbox.style.position = "fixed";
+        pg.lightbox.style.zIndex = "10";
+        pg.lightbox.style.background = "rgba(0,0,0,.8)";
+        pg.lightbox.style.top = 0;
+        pg.lightbox.style.lefts = 0;
+
+
+
     }
 }
 
