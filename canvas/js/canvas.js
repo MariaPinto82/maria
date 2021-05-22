bdb53a9de09f8aa98add42279281d8e9e43f4277
MariaPinto <maria.fullstack@gmail.com>
2021-05-22 21:30:34 +0200

    creando el oceano

--- canvas/js/canvas.js
+++ canvas/js/canvas.js
@@ -1,10 +1,18 @@
 let canvas = document.querySelector("#lienzo");
 let ctx = canvas.getContext('2d');
-
+let alfa = 1;
 
 // Cielo
 let grd = ctx.createLinearGradient(0, 0, 0, 500);
-grd.addColorStop(0, "blue");
+grd.addColorStop(0, "rgba(0,0,225," + alfa + ")");
 grd.addColorStop(1, "white");
 ctx.fillStyle = grd;
-ctx.fillRect(0, 0, 1000, 600);
+ctx.fillRect(0, 0, 1000, 500);
+
+
+// Oceano
+let grd2 = ctx.createLinearGradient(0, 400, 0, 500);
+grd2.addColorStop(0, "rgba(0,180,225," + alfa + ")");
+grd2.addColorStop(1, "white");
+ctx.fillStyle = grd2;
+ctx.fillRect(0, 400, 1000, 100);
