80421d32d4d1dca06038cf396a44215612578db7
MariaPinto <maria.fullstack@gmail.com>
2021-05-22 18:35:47 +0200

    lienzo acabado

--- canvas/js/script.js
+++ canvas/js/script.js
@@ -1,6 +1,6 @@
 let contenedor = document.querySelector("#contenedor")
 let ampliarLienzo = document.querySelector("#lienzo")
-
+let btnAmpliar = document.querySelector("#btnAmpliar")
 
 function ampliar() {
 
@@ -13,4 +13,33 @@ function ampliar() {
     ampliarLienzo.style.height = "100vh";
     ampliarLienzo.style.backgroundSize = "cover";
     ampliarLienzo.style.backgroundRepeat = "no-repeat";
+
+
+    btnAmpliar.innerHTML = "Reducir Canvas";
+    btnAmpliar.style.position = "fixed";
+    btnAmpliar.style.top = "10px";
+    btnAmpliar.style.left = "10px";
+    btnAmpliar.style.zIndex = "1";
+
+    btnAmpliar.setAttribute("onClick", "reducir()");
+}
+
+function reducir() {
+
+    contenedor.style.width = "1000px";
+    contenedor.style.height = "500px";
+    contenedor.style.margin = "5vh auto";
+
+
+    ampliarLienzo.style.width = "1000px";
+    ampliarLienzo.style.height = "500px";
+
+
+    btnAmpliar.innerHTML = "Ampliar Canvas";
+    btnAmpliar.style.position = "relative";
+    btnAmpliar.style.top = "0";
+    btnAmpliar.style.left = "0";
+    btnAmpliar.style.zIndex = "0";
+
+    btnAmpliar.setAttribute("onClick", "ampliar()");
 }
