3a6f22191993bda6b7dc3b5488e229e17fcf4b78
MariaPinto <maria.fullstack@gmail.com>
2021-06-16 21:24:37 +0200

    empezando de cero

--- php/01-hola-mundo/index.php
+++
@@ -1,7 +0,0 @@
-<h1>Hola María, esto es php </hy1><br>
-
-<?php  
-
-echo '<h2>Hola mundo, con php</h2>' // echo sirve para insertar texto dentro del documento HTML subyacente al codigo PHP
-
-?> 
--- php/13-includes/includes/footer.php
+++
@@ -1,9 +0,0 @@
-
-    <!--Pie de pagina-->
-    <footer>
-        <hr />
-        Todos los derechos reservados &copy; Maria Leonor Pinto Sabater <?= date('Y') ?>;
-    </footer>
-</body>
-
-</html>
