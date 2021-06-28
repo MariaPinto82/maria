6da90e13c4ed4aaf795dc702ec4399a1c3fffb02
MariaPinto <maria.fullstack@gmail.com>
2021-06-28 21:30:05 +0200

    creando alerts

--- js/hola-mundo/hola-mundo.js
+++ js/hola-mundo/hola-mundo.js
@@ -1,1 +1,8 @@
 'use strict';
+alert("hola mundo");
+alert("bienvenido a mi master");
+alert("con Maria Leonor");
+alert("Pinto Sabater");
+document.write("Hola mundo desde el fichero externo"); // Escribe en el html
+console.log("Muestra esto en la consola"); // Muestra en la consola
+console.log(88 + 4);
